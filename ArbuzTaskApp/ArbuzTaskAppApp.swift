24e0545efdcfe815cf66edee28feb5b2a627a4d4
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-05-21 13:23:42 +0600

    Initial Commit

---
+++ ArbuzTaskApp/ArbuzTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArbuzTaskAppApp.swift
+//  ArbuzTaskApp
+//
+//  Created by siberianarg on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ArbuzTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
