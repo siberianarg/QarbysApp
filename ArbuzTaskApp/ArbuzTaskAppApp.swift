d89f94d32c23b64d514fa2ef285deec6457bec5c
Zhan <galymzhan.aitkaliyev@gmail.com>
2023-05-22 19:15:14 +0600

    import Firebase implemented func didFinishLaunchingWithOptions

--- ArbuzTaskApp/ArbuzTaskAppApp.swift
+++ ArbuzTaskApp/ArbuzTaskAppApp.swift
@@ -6,14 +6,30 @@
 //
 
 import SwiftUI
+import Firebase
 
 let screen = UIScreen.main.bounds
 
 @main
 struct ArbuzTaskAppApp: App {
+    
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
             MainTabBar()
+        }
+    }
+    
+    class AppDelegate: NSObject, UIApplicationDelegate {
+        
+        //UIKit method is connecting to Firebase
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            
+            FirebaseApp.configure()
+            print("OK!")
+            
+            return true
         }
     }
 }
